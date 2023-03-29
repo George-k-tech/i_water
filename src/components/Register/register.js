703f502aea1fa9ca406c6f9dfd0311627b0b37c8
George-k-tech <kariukigeorge511@gmail.com>
2023-03-29 20:18:23 +0300

    changed the form inputs

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,50 +1,21 @@
-import { useState } from "react";
 
  function Register() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [password, setPassword] = useState('');
-    const [ConfirmPassword, setConfirmPassword] = useState('')
-
-    function handleNameChange(e){
-        setName(e.target.value)
-    }
-    function handleEmailChange(e){
-        setEmail(e.target.value)
-    }
-    function handlePhoneChange(e){
-        setPhone(e.target.value)
-    }
-    function handlePasswordChange(e){
-        setPassword(e.target.value)
-    }
-    function handleConfirmPasswordChange(e){
-        setConfirmPassword(e.target.value)
-    }
     
     return (
         <div>
-            <h1>The Details in the Register form are as follows:</h1>
+           Register
             <div className="register">
-                    <label >Name:{''}</label><br></br>
-                    <input  value={name} onChange ={handleNameChange}/><br></br>
-                    <label >Email:{''}</label><br></br>
-                    <input  value={email} onChange ={handleEmailChange}/><br></br>
-                    <label >Phone Number:{''}</label><br></br>
-                    <input  value={phone} onChange = {handlePhoneChange}/><br></br>
-                    <label >password:{''}</label><br></br>
-                    <input  value={password} onChange = {handlePasswordChange}/><br></br>
-                    <label >Confirm password:{''}</label><br></br>
-                    <input value={ConfirmPassword} onChange = {handleConfirmPasswordChange}/><br></br>
-                    <div>
-                        {name}
-                        {email}
-                        {phone}
-                        {password}
-                        {ConfirmPassword}
-                    </div>
-                   {/*  <button onClick={handleSubmit}>Submit</button> */}
+                    <label >Name</label><br></br>
+                    <input placeholder="input full name" /><br></br>
+                    <label >Email:</label><br></br>
+                    <input  placeholder="input email"/><br></br>
+                    <label >Phone Number:</label><br></br>
+                    <input  placeholder="input your phone number"/><br></br>
+                    <label >password:</label><br></br>
+                    <input  placeholder="input password"/><br></br>
+                    <label >Confirm password:</label><br></br>
+                    <input placeholder="confirm password"/><br></br>
+                    <button type="submit">Submit</button>
             </div>
         </div>
     );
