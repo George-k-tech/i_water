441a9f1f7fef605dbdf577c23b857e0b47c93243
George-k-tech <kariukigeorge511@gmail.com>
2023-03-29 21:42:16 +0300

    update the register to have some values

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,24 +1,35 @@
+import { useState } from "react";
 
- function Register() {
-    
+function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(name);
+        console.log(email);
+        console.log(phone);
+        console.log(password);
+    }
+
     return (
         <div>
-           Register
-            <div className="register">
-                    <label >Name</label><br></br>
-                    <input placeholder="input full name" /><br></br>
-                    <label >Email:</label><br></br>
-                    <input  placeholder="input email"/><br></br>
-                    <label >Phone Number:</label><br></br>
-                    <input  placeholder="input your phone number"/><br></br>
-                    <label >password:</label><br></br>
-                    <input  placeholder="input password"/><br></br>
-                    <label >Confirm password:</label><br></br>
-                    <input placeholder="confirm password"/><br></br>
-                    <button type="submit">Submit</button>
-            </div>
+            Register
+            <form onSubmit={handleSubmit}>
+                <label >Name</label><br></br>
+                <input placeholder="input full name" value={name} onChange={e => setName(e.target.value)} /><br></br>
+                <label >Email:</label><br></br>
+                <input placeholder="input email" value={email} onChange={e => setEmail(e.target.value)} /><br></br>
+                <label >Phone Number:</label><br></br>
+                <input placeholder="input your phone number" value={phone} onChange={e => setPhone(e.target.value)} /><br></br>
+                <label >password:</label><br></br>
+                <input placeholder="input password" value={password} onChange={e => setPassword(e.target.value)} /><br></br>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
 
-export  {Register}
+export { Register }
