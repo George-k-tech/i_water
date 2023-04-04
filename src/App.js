bb869d89bb8fbc416aced1571f7180278ef35986
George-k-tech <kariukigeorge511@gmail.com>
2023-04-05 01:16:29 +0300

    added a footer

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { Contact } from "./components/Home/Contact/contact";
 import { Cart } from "./components/Home/Cart/cart";
 import { Navbar } from "./components/Home/Navbar/navbar";
 import { Product } from "./components/Home/products/product";
+import { Footer } from "./components/Home/Footer/footer";
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/contacts' element={<Contact />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/products' element={<Product />} />
-    </Routes>
-
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contacts' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/products' element={<Product />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
