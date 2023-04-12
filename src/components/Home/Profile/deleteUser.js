6aec997464c84f8df2be9e0a5728a02bf7b33bf3
George-k-tech <kariukigeorge511@gmail.com>
2023-04-12 23:26:59 +0300

    firestore

--- src/components/Home/Profile/deleteUser.js
+++
@@ -1,7 +0,0 @@
-function DeleteUser(){
-    return(
-        <h1>hello george please delete a user </h1>
-    );
-}
-
-export  {DeleteUser};
--- src/components/Home/Profile/editUser.js
+++
@@ -1,8 +0,0 @@
-function EditUser(){
-    return (
-        <h1>hello george please edit the users below</h1>
-    );
-
-}
-
-export  {EditUser};
